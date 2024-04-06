5e3003250fecca6a85675cc5208d8e3958ef6ae5
afkdude <kumarjordy12@gmail.com>
2024-04-07 02:25:49 +0530

    added delete functionality with styles

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-let todoList = [];
+let todoList = ['first','second','third'];
 displayItems();
 
 
-function addtodo() {
-  let inputElement = document.querySelector('#todo-input');
-  let todoItem = inputElement.value;
-  todoList.push(todoItem);
-  inputElement.value = '';
 
-  displayItems();
+// add functionality 
+
+function addtodo() {
+  let inputElement = document.querySelector('#todo-input');  //selecting the input element 
+  let todoItem = inputElement.value;  //targetting the value inside the input element and storing 
+  todoList.push(todoItem);  //pusing the todo ask from input into the todoList 
+  inputElement.value = ''; //clearing the input field after the input 
+
+  displayItems();   //calling the display function for displaying 
 }
 
 
 
 function displayItems() {
-  let displayElement = document.querySelector('#todo-items');
-  displayElement.innerText = '';
-  for (let i = 0; i < todoList.length; i++){
-    displayElement.innerText = displayElement.innerText + "\n" +todoList[i];
+  let containerElement = document.querySelector('.todo-container');
+
+  let newHtml = ''; // An empty string is created to hold the HTML that will be dynamically generated.
+
+  for (let i = 0; i < todoList.length; i++) {
+
+
+    newHtml += ` 
+    <div class = "del-container">
+     <span>${todoList[i]}</span>
+     <button id="del-btn"onclick = " todoList.splice(${i},1);
+     displayItems();">Delete</button>
+    </div>
+     `;
   }
+  containerElement.innerHTML = newHtml;
   
 }
