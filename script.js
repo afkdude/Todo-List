ad6adb7da066a39cb10af30df25f49dbb90aacf0
afkdude <kumarjordy12@gmail.com>
2024-04-08 22:19:46 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let todoList = ['first','second','third'];
+// let todoList = ['first','second','third'];
+
+// updating the string array into a array of object
+
+
+let todoList = [
+  {
+    item: 'Buy Milk',
+    dueDate: '04/10/2023',
+  },
+  {
+    item: 'Do homework',
+    dueDate: '04/10/2023',
+  },
+  {
+    item: 'Go to Gym',
+    dueDate: '04/10/2023',
+  }
+];
 displayItems();
 
 
@@ -6,10 +24,23 @@ displayItems();
 // add functionality 
 
 function addtodo() {
-  let inputElement = document.querySelector('#todo-input');  //selecting the input element 
+  let inputElement = document.querySelector('#todo-input');  //selecting the input element
+
+  let dateElement = document.querySelector('#todo-date');  //date element
+
   let todoItem = inputElement.value;  //targetting the value inside the input element and storing 
-  todoList.push(todoItem);  //pusing the todo ask from input into the todoList 
+  let todoDate = dateElement.value;
+
+  // todoList.push(todoItem);  pusing the todo ask from input into the todoList
+
+  todoList.push(
+    {
+      item: todoItem,
+      dueDate: todoDate,
+    }); //updated push statement according to object
+
   inputElement.value = ''; //clearing the input field after the input 
+  dateElement.value = '';
 
   displayItems();   //calling the display function for displaying 
 }
@@ -23,15 +54,19 @@ function displayItems() {
 
   for (let i = 0; i < todoList.length; i++) {
 
+    let { item, dueDate } = todoList[i]; //destructing 
 
     newHtml += ` 
     <div class = "del-container">
-     <span>${todoList[i]}</span>
-     <button id="del-btn"onclick = " todoList.splice(${i},1);
-     displayItems();">Delete</button>
+      
+         <span>${item}</span>
+         <span>${dueDate}</span>
+         <button id="del-btn"onclick = " todoList.splice(${i},1);
+           displayItems();">Delete</button>
+      
     </div>
      `;
   }
   containerElement.innerHTML = newHtml;
-  
+
 }
